bd7f28760ffca730b09f2b3dbdee5a21c6cb3313
Jêrôme Poichet <jerome@kingofsofa.org>
2020-11-30 18:05:02 -0800

    Fix multiple to fields

--- saas/mailer.py
+++ saas/mailer.py
@@ -13,8 +13,10 @@ def send_multi_mail(subject_template_name, email_template_name,
     subject = ''.join(subject.splitlines())
     body = render_to_string(email_template_name, context)
 
+    if not isinstance(to_email, list):
+        to_email = [to_email]
     email_message = EmailMultiAlternatives(
-        subject, body, from_email, [to_email])
+        subject, body, from_email, to_email)
     if html_email_template_name is not None:
         html_email = render_to_string(html_email_template_name, context)
         email_message.attach_alternative(html_email, 'text/html')
