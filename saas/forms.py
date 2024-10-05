9449af5e12153e0d276abad8c888cb2ffd3c6439
Jêrôme Poichet <poitch@gmail.com>
2024-10-05 13:00:02 -0700

    Upgrade

--- saas/forms.py
+++ saas/forms.py
@@ -1,13 +1,17 @@
-from captcha.fields import ReCaptchaField
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.tokens import (
+    PasswordResetTokenGenerator,
+    default_token_generator,
+)
 from django.core.exceptions import ValidationError
-from django.contrib.auth.tokens import default_token_generator, PasswordResetTokenGenerator
 from django.http import HttpRequest
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
-from saas.mailer import send_multi_mail, AbstractSaasMailer
+from django_recaptcha.fields import ReCaptchaField
+
+from saas.mailer import AbstractSaasMailer, send_multi_mail
 
 User = get_user_model()
 
@@ -29,17 +33,17 @@ class CreateUserForm(UserCreationForm):
 
     def save(
         self,
-        commit:bool=True,
-        domain_override:str=None,
-        subject_template_name:str="registration/activation_subject.txt",
-        email_template_name:str="registration/activation_email.txt",
-        use_https:bool=False,
-        token_generator:PasswordResetTokenGenerator=default_token_generator,
-        from_email:str=None,
-        request:HttpRequest=None,
-        html_email_template_name:str=None,
-        extra_email_context:dict={},
-        mailer:AbstractSaasMailer=None,
+        commit: bool = True,
+        domain_override: str = None,
+        subject_template_name: str = "registration/activation_subject.txt",
+        email_template_name: str = "registration/activation_email.txt",
+        use_https: bool = False,
+        token_generator: PasswordResetTokenGenerator = default_token_generator,
+        from_email: str = None,
+        request: HttpRequest = None,
+        html_email_template_name: str = None,
+        extra_email_context: dict = {},
+        mailer: AbstractSaasMailer = None,
     ):
         user = super(CreateUserForm, self).save(commit=False)
         user.username = user.email
@@ -54,9 +58,13 @@ class CreateUserForm(UserCreationForm):
         else:
             context = {
                 "request": request,
-                "protocol": 'https' if use_https else request.scheme,
+                "protocol": "https" if use_https else request.scheme,
                 "username": self.cleaned_data.get("username"),
-                "domain": domain_override if domain_override is not None else request.META["HTTP_HOST"],
+                "domain": (
+                    domain_override
+                    if domain_override is not None
+                    else request.META["HTTP_HOST"]
+                ),
                 "uid": urlsafe_base64_encode(force_bytes(user.id)),
                 "token": token_generator.make_token(user),
             } | extra_email_context
